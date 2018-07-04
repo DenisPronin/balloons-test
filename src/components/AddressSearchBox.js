21d2a7138d845f11b91185d53f16c2a444b00722
denis pronin <pronin.idenis@gmail.com>
2018-07-04 17:44:56 +0700

    implement google search

---
+++ src/components/AddressSearchBox.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
+import classnames from 'classnames';
+
+class AddressSearchBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: '',
+      placeId: '',
+      errorMessage: '',
+      isGeocoding: false,
+    };
+  }
+
+  handleChange = address => {
+    this.setState({
+      address,
+      errorMessage: '',
+    });
+  };
+
+  handleSelect = (selected, placeId) => {
+    this.setState({ isGeocoding: true, address: selected, placeId });
+    geocodeByAddress(selected)
+      .then(res => getLatLng(res[0]))
+      .then(({ lat, lng }) => {
+        this.setState({
+          isGeocoding: false,
+        });
+      })
+      .catch(error => {
+        this.setState({ isGeocoding: false });
+        console.log('error', error);
+      });
+  };
+
+  handleCloseClick = () => {
+    this.setState({
+      address: ''
+    });
+  };
+
+  handleError = (status, clearSuggestions) => {
+    console.log('Error from Google Maps API', status); // eslint-disable-line no-console
+    this.setState({ errorMessage: status }, () => {
+      clearSuggestions();
+    });
+  };
+
+  render() {
+    const { address, errorMessage } = this.state;
+
+    return (
+      <div>
+        <PlacesAutocomplete
+          onChange={this.handleChange}
+          value={address}
+          onSelect={this.handleSelect}
+          onError={this.handleError}
+          shouldFetchSuggestions={address.length > 2}
+        >
+          {({ getInputProps, suggestions, getSuggestionItemProps }) => {
+            return (
+              <div className="Demo__search-bar-container">
+                <div className="Demo__search-input-container">
+                  <label>Enter your address: </label>
+                  <input
+                    {...getInputProps({
+                      placeholder: 'Search Places...',
+                      className: 'Demo__search-input',
+                    })}
+                  />
+                  {this.state.address.length > 0 && (
+                    <button
+                      className="Demo__clear-button"
+                      onClick={this.handleCloseClick}
+                    >
+                      x
+                    </button>
+                  )}
+                </div>
+                {suggestions.length > 0 && (
+                  <div className="Demo__autocomplete-container">
+                    {suggestions.map(suggestion => {
+                      const className = classnames('Demo__suggestion-item', {
+                        'Demo__suggestion-item--active': suggestion.active,
+                      });
+
+                      return (
+                        <div
+                          key={`suggestion-${suggestion.id}`}
+                          {...getSuggestionItemProps(suggestion, { className })}
+                        >
+                          <strong>
+                            {suggestion.formattedSuggestion.mainText}
+                          </strong>{' '}
+                          <small>
+                            {suggestion.formattedSuggestion.secondaryText}
+                          </small>
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
+            );
+          }}
+        </PlacesAutocomplete>
+
+        {errorMessage.length > 0 && (
+          <div className="Demo__error-message">{this.state.errorMessage}</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default AddressSearchBox;
